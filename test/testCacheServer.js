bd7518f25e7db765f87afae6a3e87453093ccdc1
yefuwang <m@yefu.org>
2016-02-20 12:49:36 -0500

    Improved UT coverage

--- test/testCacheServer.js
+++ test/testCacheServer.js
@@ -16,14 +16,94 @@ var mockParentSuccess={
 
 describe('CacheServer',function(){
     describe('Cache can be disabled',function(){
-        it("Return 200", function(){
+
+        it("Cache Size: 0", function(){
             var cacheServer = new CacheServer();
             cacheServer.setParent(mockParentSuccess);
+            cacheServer._lastReqFromMemory.should.equal(false);
+
             cacheServer.serveRequest({url:'http://fake.com/fake2.jpg'}, { writeHead:function(code, headObj){
                 code.should.equal(200);
             },end:function(content, type){
                 content.should.equal("Mocked Object Success");
             } });
+
+            cacheServer._lastReqFromMemory.should.equal(false);
+
+            //Request the same thing again, it should not be from the memory because the cache size is 0:
+
+            cacheServer.serveRequest({url:'http://fake.com/fake2.jpg'}, { writeHead:function(code, headObj){
+                code.should.equal(200);
+            },end:function(content, type){
+                content.should.equal("Mocked Object Success");
+            } });
+
+            cacheServer._lastReqFromMemory.should.equal(false);
+        });
+
+        it("Cache Size: 25", function(){
+            var cacheServer = new CacheServer(25);
+            cacheServer.setParent(mockParentSuccess);
+            cacheServer._lastReqFromMemory.should.equal(false);
+
+            cacheServer.serveRequest({url:'http://fake.com/fake2.jpg'}, { writeHead:function(code, headObj){
+                code.should.equal(200);
+            },end:function(content, type){
+                content.should.equal("Mocked Object Success");
+            } });
+
+            cacheServer._lastReqFromMemory.should.equal(false);
+
+            //Request the same thing again, it should be from the memory because the cache size is enough:
+            cacheServer.serveRequest({url:'http://fake.com/fake2.jpg'}, { writeHead:function(code, headObj){
+                code.should.equal(200);
+            },end:function(content, type){
+                content.should.equal("Mocked Object Success");
+            } });
+
+            cacheServer._lastReqFromMemory.should.equal(true);
+
+            cacheServer.serveRequest({url:'http://fake.com/fake3.jpg'}, { writeHead:function(code, headObj){
+                code.should.equal(200);
+            },end:function(content, type){
+                content.should.equal("Mocked Object Success");
+            } });
+            cacheServer.serveRequest({url:'http://fake.com/fake4.jpg'}, { writeHead:function(code, headObj){
+                code.should.equal(200);
+            },end:function(content, type){
+                content.should.equal("Mocked Object Success");
+            } });
+            cacheServer.serveRequest({url:'http://fake.com/fake5.jpg'}, { writeHead:function(code, headObj){
+                code.should.equal(200);
+            },end:function(content, type){
+                content.should.equal("Mocked Object Success");
+            } });
+            cacheServer.serveRequest({url:'http://fake.com/fake3.jpg'}, { writeHead:function(code, headObj){
+                code.should.equal(200);
+            },end:function(content, type){
+                content.should.equal("Mocked Object Success");
+            } });
+            cacheServer.serveRequest({url:'http://fake.com/fake4.jpg'}, { writeHead:function(code, headObj){
+                code.should.equal(200);
+            },end:function(content, type){
+                content.should.equal("Mocked Object Success");
+            } });
+            cacheServer.serveRequest({url:'http://fake.com/fake5.jpg'}, { writeHead:function(code, headObj){
+                code.should.equal(200);
+            },end:function(content, type){
+                content.should.equal("Mocked Object Success");
+            } });
+
+            cacheServer.serveRequest({url:'http://fake.com/fake2.jpg'}, { writeHead:function(code, headObj){
+                code.should.equal(200);
+
+            },end:function(content, type){
+                content.should.equal("Mocked Object Success");
+            } });
+
+            cacheServer._lastReqFromMemory.should.equal(false);
+
+
         });
     });
 });
