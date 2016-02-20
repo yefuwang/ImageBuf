53d2c0cf203391724d00d92373d007b297aceba0
yefuwang <m@yefu.org>
2016-02-20 11:33:00 -0500

    Improved UT coverage

--- test/testCacheServer.js
+++ test/testCacheServer.js
@@ -6,16 +6,24 @@ var assert = require('assert');
 var should = require('should');
 
 var CacheServer = require('../lib/cacheServer');
-/*
-var cacheServer = new CacheServer(cacheSize);
 
-describe('ImageBufUtils',function(){
-    describe('parseSize',function(){
-        it("Size in Bytes", function(){
-            imageBufUtils.parseSize(1000).should.equal(1000);
-            imageBufUtils.parseSize('1000').should.equal(1000);
+// Mocked object to feed data to the local cache
+var mockParentSuccess={
+    load:function(fileName, onSuccess, onError){
+        onSuccess("Mocked Object Success");
+    },
+}
+
+describe('CacheServer',function(){
+    describe('Cache can be disabled',function(){
+        it("Return 200", function(){
+            var cacheServer = new CacheServer();
+            cacheServer.setParent(mockParentSuccess);
+            cacheServer.serveRequest({url:'http://fake.com/fake2.jpg'}, { writeHead:function(code, headObj){
+                code.should.equal(200);
+            },end:function(content, type){
+                content.should.equal("Mocked Object Success");
+            } });
         });
-
     });
 });
-*/
