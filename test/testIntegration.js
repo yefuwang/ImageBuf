ddce265d081ab5d3133b429e9f40d0f9e4678262
yefuwang <m@yefu.org>
2016-02-21 08:44:28 -0500

    Add integration test

--- test/testIntegration.js
+++ test/testIntegration.js
@@ -23,12 +23,29 @@ describe('ImageBuf Integration',function(){
     });
 
     describe('http request',function(){
-        it("First Request", function(){
+
+        this.timeout(5000);
+
+        it("First Request", function(done){
             binRequest('http://127.0.0.1:1337/2015/dec/birthday/DSC_0002.JPG', function (error, response, body) {
-                //console.log(response);
-                error.should.equal(false);
+                console.log('Status:' + response.statusCode);
+                console.log(error);
+                console.log(body.length);
                 response.statusCode.should.equal(200);
-                body.length.should.notEqual(0);
+                assert(body.length!=0)
+                body.length.should.equal(4994102);
+                done();
+            });
+        });
+
+        it("Second Request: etag not used ", function(done){
+            console.log("Before the 2nd ");
+            binRequest('http://127.0.0.1:1337/2015/dec/birthday/DSC_0002.JPG', function (error, response, body) {
+                console.log('Status:' + response.statusCode);
+                response.statusCode.should.equal(200);
+                console.log(body.length);
+                body.length.should.not.equal(0);
+                done();
             });
         });
     });
